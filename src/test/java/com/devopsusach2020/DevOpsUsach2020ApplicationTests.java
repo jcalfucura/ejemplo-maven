980cdd2ccab300275afe8f561977e9a1c0cf5ca3
Juan Calfucura <juan.calfucura@gmail.com>
2022-01-13 22:01:09 -0300

    assert fixed

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -2,12 +2,14 @@ package com.devopsusach2020;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.assertNull(null);
 	}
 
 }
